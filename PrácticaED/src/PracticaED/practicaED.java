8e8bb51a26b320a1a5170a12c9c051a17ca08a6d
Pablo <Pablo@DESKTOP-C95QR6F>
2018-03-10 09:32:16 +0100

    Commit a GitHub

--- PrácticaED/src/PracticaED/practicaED.java
+++ PrácticaED/src/PracticaED/practicaED.java
@@ -9,13 +9,15 @@ public class practicaED
 		int b;
 		int c;
 		int z;
+		int x;
 		
 		a=1;
 		b=2;
 		c=3;
 		z=20;
+		x=1;
 		
-		System.out.println(a+b*c+z);
+		System.out.println(a+b*c+z-1);
 
 	}
 
