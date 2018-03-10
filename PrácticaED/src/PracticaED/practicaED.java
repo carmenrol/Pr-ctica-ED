3797e77c4851ae50b5af38a371fbc16a3e751343
Pablo <Pablo@DESKTOP-C95QR6F>
2018-03-10 09:18:12 +0100

    1º Commit

---
+++ PrácticaED/src/PracticaED/practicaED.java
@@ -0,0 +1,20 @@
+package PracticaED;
+
+public class practicaED
+{
+
+	public static void main(String[] args)
+	{
+		int a;
+		int b;
+		int c;
+		
+		a=1;
+		b=2;
+		c=3;
+		
+		System.out.println(a+b+c);
+
+	}
+
+}
